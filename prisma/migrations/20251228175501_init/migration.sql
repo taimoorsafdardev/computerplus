889fe2b4f7b9661fe697c06890e76e6a403c6c83
taimoorsafdardev <starxthorn@gmail.com>
2025-12-28 22:58:06 +0500

    .env change = db acc + redis acc changing

--- prisma/migrations/20251228175501_init/migration.sql
+++ prisma/migrations/20251228175501_init/migration.sql
@@ -62,8 +62,13 @@ CREATE TABLE "Order" (
 CREATE TABLE "OrderItem" (
     "id" TEXT NOT NULL,
     "quantity" INTEGER NOT NULL,
-    "price" TEXT NOT NULL,
-    "productId" TEXT NOT NULL,
+    "productName" TEXT NOT NULL,
+    "productImage" TEXT NOT NULL,
+    "unitPrice" TEXT NOT NULL,
+    "discount" TEXT,
+    "discountPrice" TEXT,
+    "total" TEXT NOT NULL,
+    "productId" TEXT,
     "orderId" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
@@ -81,7 +86,7 @@ CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
 ALTER TABLE "Order" ADD CONSTRAINT "Order_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "OrderItem" ADD CONSTRAINT "OrderItem_productId_fkey" FOREIGN KEY ("productId") REFERENCES "Product"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "OrderItem" ADD CONSTRAINT "OrderItem_productId_fkey" FOREIGN KEY ("productId") REFERENCES "Product"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "OrderItem" ADD CONSTRAINT "OrderItem_orderId_fkey" FOREIGN KEY ("orderId") REFERENCES "Order"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
